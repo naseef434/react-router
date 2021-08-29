c3905f80158a2051b480650537bc8b265dbfc077
naseef434 <naseefp.hin@gmail.com>
2021-08-30 00:03:36 +0530

    connected a api

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
+
 function Shop() {
+
+    useEffect(() => {
+        fetchItems();
+    },[]);
+
+    const [items, setItems] = useState([]);
+    
+    const fetchItems = async () => {
+        const data = await fetch('https://fortnite-api.com/v1/banners');
+        const items = await data.json();
+        console.log(items.data)
+        setItems(items.data);
+    }
   return (
     <div>
-     <h1>This is Shop</h1>
+     {items.map(item =>(
+         <h1>{item.devName}</h1>
+     ))}
     </div>
   );
 }
